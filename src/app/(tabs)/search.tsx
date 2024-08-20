4e49510e970be9e7fc8a4ac5215548703ec9043d
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-08-20 15:31:44 -0300

    create search screen

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SearchIcon } from "lucide-react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="w-full flex-1 items-center bg-neutral-900 px-4">
+      <View className="w-full flex-row items-center justify-between bg-neutral-800 h-12 px-3 rounded-2xl mt-10">
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          className="h-12 text-white w-[322px]"
+          placeholder="Pesquisar"
+          placeholderTextColor={"gray"}
+        />
+        <SearchIcon size={28} color={"gray"} />
+      </View>
     </View>
   );
 }
