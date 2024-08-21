470f8154027dfafbaf16e534106c220f34db02e7
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-08-21 16:04:29 -0300

    change header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,20 @@ import { View, Text } from "react-native";
 type HeaderProps = {
   label: string;
 };
+
 export default function Header({ label }: HeaderProps) {
   return (
     <View className="w-full flex-col justify-center items-center bg-violet-600 h-24 px-4">
       <View className="w-full flex-row items-center justify-between">
-        <ChevronLeft onPress={router.back} size={28} color={"#ffff"} />
-        <Text className="text-white font-bold text-lg">{label}</Text>
-        <Snowflake size={28} color={"#fff"} />
+        <View className="p-3 bg-violet-500 rounded-2xl items-center justify-center">
+          <ChevronLeft onPress={router.back} size={28} color={"#ffff"} />
+        </View>
+        <Text className="text-white font-medium text-xl items-center justify-center">
+          {label}
+        </Text>
+        <View className="p-3 bg-violet-500 rounded-2xl">
+          <Snowflake size={28} color={"#fff"} />
+        </View>
       </View>
     </View>
   );
