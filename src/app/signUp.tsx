c784d9eb0801b79acd02aea5beca1be13f147aac
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-08-22 16:34:41 -0300

    adjust signUp screen

--- src/app/signUp.tsx
+++ src/app/signUp.tsx
@@ -1,16 +1,41 @@
 import { ScrollView, StatusBar, Text, ToastAndroid, View } from "react-native";
 import LottieView from "lottie-react-native";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button } from "@/components/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/Input";
 import { useForm, Controller } from "react-hook-form";
 import { registerSchema, RegisterSchema } from "@/schemas/registerSchema";
 import { useAuth } from "@/store/auth";
+import { Select } from "@/components/Select";
+import { Grid } from "@/components/Grid";
 
 export default function SignUp() {
   const lottieRef = useRef<LottieView>(null);
   const { singUp, success, successLabel } = useAuth();
+  const [value, setValue] = useState("");
+  const jobType = [
+    {
+      label: "Desenvolvedor",
+      value: "developer",
+    },
+    {
+      label: "Suporte Técnico",
+      value: " technical_support",
+    },
+    {
+      label: "Gerente",
+      value: "manager",
+    },
+    {
+      label: "Supervisor",
+      value: "supervisor",
+    },
+    {
+      label: "Técnico",
+      value: "technical",
+    },
+  ];
 
   const {
     handleSubmit,
@@ -37,12 +62,12 @@ export default function SignUp() {
   }
 
   return (
-    <View className="w-full flex-1 items-center mx-auto bg-black">
+    <View className="w-full flex-1 items-center mx-auto bg-neutral-900">
       <StatusBar barStyle={"light-content"} />
       <View className="w-full items-center justify-center bg-violet-600 rounded-b-[50px]">
         <LottieView
           ref={lottieRef}
-          style={{ width: 250, height: 250 }}
+          style={{ width: 220, height: 220 }}
           source={require("@/lotties/mobile.json")}
           autoPlay
         />
@@ -54,72 +79,81 @@ export default function SignUp() {
       </View>
 
       <ScrollView className="w-full">
-        <View className="w-full flex-col items-center px-4 ">
-          <View className="w-full flex-col my-2">
-            <Controller
-              control={control}
-              name="username"
-              render={({ field: { onChange } }) => (
-                <Input
-                  onChangeText={onChange}
-                  label="Nome de usuário"
-                  placeholder="Criar nome de usuário"
-                  error={errors.username}
-                  isValid={!errors.username && !!watch("username")}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name="email"
-              render={({ field: { onChange } }) => (
-                <Input
-                  onChangeText={onChange}
-                  label="E-mail"
-                  placeholder="example@gmail.com"
-                  error={errors.email}
-                  isValid={!errors.email && !!watch("email")}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name="password"
-              render={({ field: { onChange } }) => (
-                <Input
-                  label="Senha"
-                  onChangeText={onChange}
-                  placeholder="Criar Senha"
-                  secureTextEntry
-                  isValid={!errors.password && !!watch("password")}
-                  error={errors.password}
-                />
-              )}
-            />
-            <Controller
-              control={control}
-              name="confirmPassword"
-              render={({ field: { onChange } }) => (
-                <Input
-                  onChangeText={onChange}
-                  label="Confirmar senha"
-                  placeholder="Confirmar nova senha"
-                  secureTextEntry
-                  isValid={
-                    !errors.confirmPassword && !!watch("confirmPassword")
-                  }
-                  error={errors.confirmPassword}
-                />
-              )}
-            />
+        <Grid columns={1} className="w-full items-center justify-center px-4">
+          <Controller
+            control={control}
+            name="username"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                label="Nome de usuário"
+                placeholder="Criar nome de usuário"
+                error={errors.username}
+                isValid={!errors.username && !!watch("username")}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                label="E-mail"
+                placeholder="example@gmail.com"
+                error={errors.email}
+                isValid={!errors.email && !!watch("email")}
+              />
+            )}
+          />
+          <View className="w-full flex-col mb-3">
+            <Text className="font-medium text-white text-sm mb-1">Cargo</Text>
+            <View className="w-full items-center justify-center border border-neutral-700 bg-neutral-900 rounded-2xl px-1 h-14">
+              <Select
+                placeholder="Selecionar cargo"
+                mode="dropdown"
+                value={value}
+                options={jobType}
+                className="w-full text-xs text-white"
+                onChange={setValue}
+              />
+            </View>
           </View>
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange } }) => (
+              <Input
+                label="Senha"
+                onChangeText={onChange}
+                placeholder="Criar Senha"
+                secureTextEntry
+                isValid={!errors.password && !!watch("password")}
+                error={errors.password}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange } }) => (
+              <Input
+                onChangeText={onChange}
+                label="Confirmar senha"
+                placeholder="Confirmar nova senha"
+                secureTextEntry
+                isValid={!errors.confirmPassword && !!watch("confirmPassword")}
+                error={errors.confirmPassword}
+              />
+            )}
+          />
           <Button
             success={success}
             successLabel={successLabel}
             onPress={handleSubmit(onSubmit)}
             label={"Criar conta"}
           />
-        </View>
+        </Grid>
       </ScrollView>
     </View>
   );
