d6072f3f2aa5f98db8404f4d05e33c0c2ba7cbed
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-08-26 15:48:29 -0300

    change Button component, adjust any props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,3 @@
-import { CircleCheckBig, CircleX } from "lucide-react-native";
 import {
   Text,
   View,
@@ -6,14 +5,10 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   TouchableOpacityProps,
-} from "react-native";
+} from 'react-native';
 
 type ButtonProps = {
   label: string;
-  successLabel?: string;
-  errorLabel?: string;
-  error?: boolean;
-  success?: boolean;
   isLoading?: boolean;
   onPress?: (event: GestureResponderEvent) => void;
 } & TouchableOpacityProps;
@@ -21,37 +16,18 @@ type ButtonProps = {
 export function Button({
   label,
   onPress,
-  successLabel,
-  errorLabel,
-  error = false,
-  success = false,
+
   isLoading = false,
   ...rest
 }: ButtonProps) {
   return (
     <TouchableOpacity
       {...rest}
-      className={`w-full flex items-center justify-center h-14 rounded-2xl ${
-        error ? "bg-red-500" : success ? "bg-emerald-500" : "bg-violet-600"
-      }`}
+      className="w-full flex items-center justify-center h-14 rounded-2xl bg-violet-600"
       onPress={onPress}
     >
       <View className="flex-row w-full items-center justify-center">
-        {success && !isLoading && (
-          <View className="flex-row items-center gap-2">
-            <Text className="text-white font-medium">{successLabel}</Text>
-            <CircleCheckBig className="text-white text-2xl" />
-          </View>
-        )}
-        {error && !isLoading && (
-          <View className="flex-row items-center gap-2">
-            <Text className="text-white font-medium">{errorLabel}</Text>
-            <CircleX className="text-white text-2xl" />
-          </View>
-        )}
-        {!success && !error && !isLoading && (
-          <Text className="text-white font-bold text-[17px]">{label}</Text>
-        )}
+        {!isLoading && <Text className="text-white font-bold text-[17px]">{label}</Text>}
         {isLoading && <ActivityIndicator size="small" color="#fff" />}
       </View>
     </TouchableOpacity>
