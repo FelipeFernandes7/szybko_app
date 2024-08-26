8d303fa6b5470b476bd005cadc96ba01b8a53466
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-08-26 15:48:46 -0300

    ...

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
-import { Text, TextInput, TextInputProps, View } from "react-native";
-import { UseFormRegister, FieldError } from "react-hook-form";
+import { Text, TextInput, TextInputProps, View } from 'react-native';
+import { UseFormRegister, FieldError } from 'react-hook-form';
 
 interface InputProps extends TextInputProps {
   label?: string;
@@ -9,33 +9,26 @@ interface InputProps extends TextInputProps {
   register?: UseFormRegister<any>;
 }
 
-export function Input({
-  label,
-  error,
-  name,
-  isValid,
-  register,
-  ...rest
-}: InputProps) {
+export function Input({ label, error, name, isValid, register, ...rest }: InputProps) {
   return (
     <View className="w-full flex-col mb-3">
       {!!label && (
-        <Text className="font-medium text-white text-sm" aria-label={name}>
+        <Text className="font-medium text-white text-sm mb-1" aria-label={name}>
           {label}
         </Text>
       )}
       <View
         className={`w-full h-14 mt-1 ${
           error
-            ? "border-2 border-red-500"
+            ? 'border-2 border-red-500'
             : isValid
-            ? "border-2 border-emerald-500"
-            : "border-none"
+              ? 'border-2 border-emerald-500'
+              : 'border-none'
         } rounded-2xl items-center px-4 bg-transparent justify-between border border-neutral-700 bg-neutral-900`}
       >
         {register && name ? (
           <TextInput
-            placeholderTextColor={"gray"}
+            placeholderTextColor={'gray'}
             className="w-full h-14 outline-none bg-transparent text-white"
             {...register(name)}
             {...(label ? { id: name } : {})}
@@ -43,7 +36,7 @@ export function Input({
           />
         ) : (
           <TextInput
-            placeholderTextColor={"gray"}
+            placeholderTextColor={'gray'}
             className="w-full h-14 outline-none bg-transparent text-white"
             {...(label ? { id: name } : {})}
             {...rest}
@@ -51,9 +44,7 @@ export function Input({
         )}
       </View>
       {!!error && (
-        <Text className="text-red-500 mt-2 font-medium text-xs">
-          {error.message}
-        </Text>
+        <Text className="text-red-500 mt-2 font-medium text-xs">{error.message}</Text>
       )}
     </View>
   );
